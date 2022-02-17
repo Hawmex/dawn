f3e857894a91533aa3aa9f33131e7aa1ea939b83
Hawmex <hawmed.a@gmail.com>
2022-02-17 18:47:50 +0330

    v0.5.2

--- lib/dawn.dart
+++ lib/dawn.dart
@@ -1,6 +1,8 @@
 library dawn;
 
 // TODO: Refactor the entire project.
+// TODO: Add an image framework widget.
+// TODO: Add an input framework widget.
 
 export 'src/app_runner.dart';
 export 'src/context.dart';
