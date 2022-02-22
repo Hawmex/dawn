1bdf01fcde2d4edf8f1dcae01998ad48c5445d17
Hawmex <hawmed.a@gmail.com>
2022-02-22 22:59:16 +0330

    changed todo

--- lib/dawn.dart
+++ lib/dawn.dart
@@ -1,6 +1,6 @@
 library dawn;
 
-// TODO: Add an input framework widget.
+// TODO: Add an input framework widget (aim for forms in general).
 
 export 'src/app_runner.dart';
 export 'src/context.dart';
