4f756e630a91c9a697c9bb6d0c83818b1bae4b09
Hawmex <hawmed.a@gmail.com>
2022-02-24 12:47:33 +0330

    fixed dev script

--- example/scripts/dev.bat
+++ example/scripts/dev.bat
@@ -1,1 +1,1 @@
-start /b nodemon -e dart,yaml --ignore build --exec "dart compile js web/main.dart -o build/main.dart.js -m && cp web/index.html build/index.html" && start /b http://localhost:3001 && py -m http.server --dir build --bind 0.0.0.0 3001
+start /b nodemon -e dart,yaml --ignore build --exec "dart compile js web/main.dart -o build/main.dart.js && cp web/index.html build/index.html" && start /b http://localhost:3001 && py -m http.server --dir build --bind 0.0.0.0 3001
