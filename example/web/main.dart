2b628fc0fb4eac456ba0fd7f37a675478c6e5547
Hawmex <hawmed.a@gmail.com>
2022-03-04 17:16:13 +0330

    made welcome page for example

--- example/web/main.dart
+++ example/web/main.dart
@@ -9,17 +9,43 @@ class App extends StatelessWidget {
   Widget build(final Context context) {
     return const Container(
       [
+        Image(
+          '/assets/logo.svg',
+          style: Style(['width: 128px', 'height: 128px']),
+        ),
         Text(
-          'Dawn app running!',
-          style: Style(['font-size: 24px', 'font-weight: 700']),
-        )
+          'Welcome to Dawn',
+          style: Style([
+            'font-size: 24px',
+            'font-weight: bold',
+          ]),
+        ),
+        Container([
+          Text('To get started, edit '),
+          Text(
+            'web/main.dart',
+            style: Style([
+              'font-family: monospace',
+              'background: #666666',
+              'border-radius: 4px',
+              'padding: 4px',
+            ]),
+          ),
+          Text(' and save to reload.'),
+        ]),
       ],
       style: Style([
         'display: flex',
-        'width: 100vw',
-        'height: 100vh',
+        'flex-flow: column',
         'justify-content: center',
         'align-items: center',
+        'gap: 16px',
+        'width: 100%',
+        'height: 100vh',
+        'background: #000000',
+        'color: #ffffff',
+        'font-family: system-ui',
+        'user-select: none',
       ]),
     );
   }
