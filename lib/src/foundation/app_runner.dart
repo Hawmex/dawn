1298461e3d3717e2668604ba53a2faf7d2b009f7
Hawmex <hawmed.a@gmail.com>
2022-09-12 11:07:15 +0430

    6.0.0-dev.4

--- lib/src/foundation/app_runner.dart
+++ lib/src/foundation/app_runner.dart
@@ -1,7 +1,7 @@
 import 'package:dawn/widgets.dart';
 import 'package:js/js.dart';
 
-@JS()
+@JS('__dawnAppNode__')
 external Node? _appNode;
 
 /// Inflates the given widget and attaches it to the screen.
